306cf06e98c443cdea63cf09cc37aeeb894f2d37
Emanuel Aracena <earacenadev@gmail.com>
2019-01-05 21:17:56 -0500

    Added Menu class.
    Menu class will handle the items inside lists (with or without scrollbars as well as drop down lists).
    
    Signed-off-by: Emanuel Aracena <earacenadev@gmail.com>

--- menu.cpp
+++ menu.cpp
@@ -4,3 +4,15 @@
  *	Date Created: 	January 5, 2019
  *	Description:	Menu class implementation.
  */
+#include "menu.h"
+
+Menu::Menu()
+{
+	//...
+}
+
+void Menu::add_item(std::string item_name)
+{
+	//Item menu_item(item_name);
+	//items_.push_back(menu_item);
+}
--- menu.h
+++ menu.h
@@ -4,3 +4,20 @@
  *	Date Created: 	January 5, 2019
  *	Description:	Menu class interface.
  */
+
+#ifndef MENU_H
+#define MENU_H
+
+#include <string>
+#include <vector>
+
+class Menu
+{
+public:
+	Menu();
+	void add_item(std::string item_name);
+private:
+	//std::vector<Items> items_;
+};
+
+#endif	// MENU_H
